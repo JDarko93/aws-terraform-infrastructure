8b0e55d08df964645baffe441df8659370f85b57
jesse <cyberdarko24@gmail.com>
2026-02-14 16:16:58 +0000

    updated backend.tf files

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -2,15 +2,15 @@
 # Terraform State Backend Configuration
 # Uncomment and configure after creating S3 bucket and DynamoDB table
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "your-terraform-state-bucket"  # CHANGE THIS
-#     key            = "dev/terraform.tfstate"
-#     region         = "eu-west-2"
-#     encrypt        = true
-#     dynamodb_table = "terraform-state-lock"
-#   }
-# }
+ terraform {
+   backend "s3" {
+     bucket         = "aws-infra-project-2026"
+     key            = "dev/terraform.tfstate"
+     region         = "eu-west-2"
+     encrypt        = true
+     dynamodb_table = "terraform-state-lock"
+   }
+ }
 
 # To create the S3 bucket and DynamoDB table, run:
 # aws s3api create-bucket --bucket your-terraform-state-bucket --region eu-west-2
