c7ad22271ae2995e01bd4caa860c447a4a80fc14
jesse <cyberdarko24@gmail.com>
2026-02-15 16:56:47 +0000

    updated postgres version

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -29,7 +29,7 @@ resource "aws_db_parameter_group" "main" {
 resource "aws_db_instance" "main" {
   identifier     = "${var.environment}-postgres-db"
   engine         = "postgres"
-  engine_version = "15.4"
+  engine_version = "15.7"
   instance_class = var.instance_class
 
   allocated_storage     = var.allocated_storage
